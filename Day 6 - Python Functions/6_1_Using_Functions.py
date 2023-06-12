053d70c52cd050dde9abb64d0f9c4f982cc6e23f
hristo2612 <hristoapps@gmail.com>
2023-06-12 15:13:31 +0300

    Day 6 - Functions & While loops

---
+++ Day 6 - Python Functions/6_1_Using_Functions.py
@@ -0,0 +1,7 @@
+# Defining & using a function in Python
+def a_really_nice_function():
+    print("Hey i'm a really nice function! You just called me!")
+
+
+# call the function
+a_really_nice_function()
---
+++ Day 6 - Python Functions/6_2_While_Loops.py
@@ -0,0 +1,12 @@
+# While loops in Python
+some_number = 10
+while some_number > 0:
+    print(some_number)
+    some_number -= 1
+
+some_number = 11
+while not some_number == 0:
+    print(some_number)
+    some_number -= 1
+
+print("Yay!")
