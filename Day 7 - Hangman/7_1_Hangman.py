3271cfea2571dddd19bf89b95bafba6928996b72
hristo2612 <hristoapps@gmail.com>
2023-06-13 14:13:59 +0300

    Day 7 - Hangman - Challenge 2 - Replacing Blanks with Guesses

--- Day 7 - Hangman/7_1_Hangman.py
+++ Day 7 - Hangman/7_1_Hangman.py
@@ -1,18 +1,29 @@
 # An interactive hangman game you can play from the console
 import random
 
-# Step 1
 word_list = ["mustache", "giraffe", "car"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called (chosen_word)
+# Choose a random word for the user to guess
 chosen_word = random.choice(word_list)
+chosen_word_len = len(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called (guess). Make guess lowercase
-guess = input("What is you guess?\n").lower()
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the (chosen_word)
-for char in chosen_word:
-    if guess == char:
-        print("Right")
-    else:
-        print("Wrong")
+# TODO-1: - Create an empty List called (display).
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = ['_' for _ in range(chosen_word_len)]
+print(" ".join(display))
+guess = input("Guess a letter: ").lower()
+
+# TODO-2: - Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for index in range(chosen_word_len):
+    letter = chosen_word[index]
+    if guess == letter:
+        display[index] = guess
+
+# TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+print(" ".join(display))
