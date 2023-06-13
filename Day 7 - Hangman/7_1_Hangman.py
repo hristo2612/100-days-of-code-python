febaaffb66e9ecefffb336c6359c21ab270fa569
hristo2612 <hristoapps@gmail.com>
2023-06-13 14:57:06 +0300

    Day 7 - Hangman - Challenge 4 - Keep track of player lives

--- Day 7 - Hangman/7_1_Hangman.py
+++ Day 7 - Hangman/7_1_Hangman.py
@@ -1,38 +1,114 @@
 # An interactive hangman game you can play from the console
 import random
 
+# Hangman stages
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+# List of random words
 word_list = ["mustache", "giraffe", "car"]
 
 # Choose a random word for the user to guess
 chosen_word = random.choice(word_list)
 chosen_word_len = len(chosen_word)
 
+lives = 6
+
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks "_". Then we can tell the user they've won.
+# Display the blank & guessed letters
 display = ['_' for _ in range(chosen_word_len)]
 
 
 def guess():
+    # Make python know that lives is reffering to the global 'lives' variable instead of the local scope
+    global lives
     # Fill in the blanks
-    print(" ".join(display))
     guess = input("Guess a letter: ").lower()
     for index in range(chosen_word_len):
         letter = chosen_word[index]
         if guess == letter:
             display[index] = guess
 
+    if guess not in chosen_word:
+        lives -= 1
+    print(" ".join(display))
+    print(stages[lives])
 
-def guessed_all_letters():
+
+def game_ended():
+    if '_' not in display or lives == 0:
+        return True
+    else:
+        return False
+
+
+def has_won():
     if '_' not in display:
         return True
     else:
         return False
 
 
-while not guessed_all_letters():
+while not game_ended():
     guess()
 
-print(" ".join(display))
-print("You won!")
+if has_won():
+    print("You win!")
+else:
+    print("You lose.")
